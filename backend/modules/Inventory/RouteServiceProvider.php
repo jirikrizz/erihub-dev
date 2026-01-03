8d2c8db1232f4ebaa924755857efdefcd39d2af1
Jiri Kriz <jirikrizz99@gmail.com>
2026-01-03 08:25:59 +0100

    fix(inventory): move routes inline to test registration

--- backend/modules/Inventory/RouteServiceProvider.php
+++ backend/modules/Inventory/RouteServiceProvider.php
@@ -4,6 +4,8 @@ namespace Modules\Inventory;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Modules\Inventory\Http\Controllers\InventoryRecommendationWidgetController;
+use Modules\Inventory\Http\Controllers\PublicRecommendationsController;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -18,7 +20,16 @@ class RouteServiceProvider extends ServiceProvider
         Route::prefix('api')
             ->middleware('api')
             ->group(function () {
-                $this->loadRoutesFromFile(__DIR__.'/routes/public.php');
+                Route::get('test-widget', function() { 
+                    \Illuminate\Support\Facades\Log::debug('Test route accessed!');
+                    return "Widget route works!"; 
+                });
+                
+                Route::get('widgets/inventory/recommendations.js', [InventoryRecommendationWidgetController::class, 'script']);
+                
+                Route::prefix('inventory')->group(function () {
+                    Route::get('recommendations/products', [PublicRecommendationsController::class, 'products']);
+                });
             });
     }
 }
