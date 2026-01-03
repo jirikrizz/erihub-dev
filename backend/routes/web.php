fbaa30e0744feff595a02660139a94d784fe4f1d
Jiri Kriz <jirikrizz99@gmail.com>
2026-01-03 08:20:52 +0100

    feat(routing): add public widget endpoint to web routes

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Inventory\Http\Controllers\InventoryRecommendationWidgetController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Public widget endpoints
+Route::get('/api/widgets/inventory/recommendations.js', [InventoryRecommendationWidgetController::class, 'script']);
